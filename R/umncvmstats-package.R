5c0a65daad900729a427b0b492f6a5e21c7eb143
Aaron Rendahl <rend0020@umn.edu>
2024-07-11 08:50:10 -0500

    stub of package help file

---
+++ R/umncvmstats-package.R
@@ -0,0 +1,10 @@
+#' umncvmstats ...
+#'
+#' This package ...
+#'
+#' @name umncvmstats
+#' @author Aaron Rendahl, \email{rend0020@umn.edu}
+#' @seealso [one_proportion_test(), two_proportion_test()]
+#' @keywords internal
+"_PACKAGE"
+NULL
