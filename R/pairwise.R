1f8924778615e8a0708c8c6800c63ffa97837944
Aaron Rendahl <rend0020@umn.edu>
2024-07-13 18:30:47 -0500

    separate pairwise code from test_by code

--- R/pairwise.R
+++ R/pairwise.R
@@ -1,59 +1,3 @@
-#' @importFrom stats as.formula
-test_by <- function(by_right=FALSE) {
-  m <- match.call(
-    definition=sys.function(sys.parent()),
-    call=sys.call(sys.parent()),
-    expand.dots=TRUE,
-    envir=parent.frame(2L))
-  FUN <- format(m[[1]]) |> str_remove("\\.formula$")
-  formula <- as.formula(m[["formula"]])
-  params <- as.list(m)[-1]
-  params <- params[names(params)!="formula"]
-
-  ## first split the formula, if more than one found, rerun each separately
-  fs <- split_formula(formula)
-  if(length(fs) > 1) {
-    # cat("+ ", format(m), "\n")
-    return(map(fs, function(x) do.call(FUN, c(list(x), params))) |> bind_rows())
-  }
-
-  ## otherwise see if there are groups, if so, make subsets and rerun
-  ## if not, just return NULL and let the function run as is.
-  a <- parse_formula(formula=formula)
-  if(any(a$side=="group")) {
-    by_name <- "group"
-  } else if(isTRUE(by_right) && any(a$side=="right")) {
-    by_name <- "right"
-  } else {
-    # cat("> ", format(m), "\n")
-    return(NULL)
-  }
-
-  # cat("| ", format(m), "\n")
-  data <- eval(m$data)
-  f <- parse_formula(formula=formula, data=data)
-  new.name <- f$about$var.names[f$about$side==by_name]
-  subformula <- clean_formula(formula, by_name)
-  result <- data |>
-    mutate(.groups=f$data[[by_name]]) |>
-    nest(.by=".groups") |>
-    arrange(.data$.groups) |>
-    mutate(.x=map(data, \(.x) {
-      paramsi <- params
-      paramsi$data <- .x
-      paramsi <- c(list(subformula), paramsi)
-      do.call(FUN, paramsi)
-    })) |> select(-"data") |>
-    unnest(".x")
-  if(by_name=="right") {
-    result <- result |> rename(value=".groups") |> mutate(variable=new.name)
-  } else {
-    result <- result |> rename(group.value=".groups") |> mutate(group=new.name)
-  }
-  as_atest(result)
-}
-
-
 #' @importFrom stats p.adjust
 #' @importFrom utils combn
 #' @importFrom forcats fct_rev
@@ -83,7 +27,6 @@ pairwise <- function(formula, data, FUN, conf.level=0.95, ..., adjust=c("bonferr
   as_atest(result)
 }
 
-
 #' @param ... XX
 #' @rdname two_t_test
 #' @export
@@ -97,4 +40,3 @@ pairwise_t_test <- function(formula, data, ...) {
 pairwise_proportion_test <- function(formula, data, ...) {
   pairwise(formula, data, "two_proportion_test", ...)
 }
-
---
+++ R/test_by.R
@@ -0,0 +1,54 @@
+#' @importFrom stats as.formula
+test_by <- function(by_right=FALSE) {
+  m <- match.call(
+    definition=sys.function(sys.parent()),
+    call=sys.call(sys.parent()),
+    expand.dots=TRUE,
+    envir=parent.frame(2L))
+  FUN <- format(m[[1]]) |> str_remove("\\.formula$")
+  formula <- as.formula(m[["formula"]])
+  params <- as.list(m)[-1]
+  params <- params[names(params)!="formula"]
+
+  ## first split the formula, if more than one found, rerun each separately
+  fs <- split_formula(formula)
+  if(length(fs) > 1) {
+    # cat("+ ", format(m), "\n")
+    return(map(fs, function(x) do.call(FUN, c(list(x), params))) |> bind_rows())
+  }
+
+  ## otherwise see if there are groups, if so, make subsets and rerun
+  ## if not, just return NULL and let the function run as is.
+  a <- parse_formula(formula=formula)
+  if(any(a$side=="group")) {
+    by_name <- "group"
+  } else if(isTRUE(by_right) && any(a$side=="right")) {
+    by_name <- "right"
+  } else {
+    # cat("> ", format(m), "\n")
+    return(NULL)
+  }
+
+  # cat("| ", format(m), "\n")
+  data <- eval(m$data)
+  f <- parse_formula(formula=formula, data=data)
+  new.name <- f$about$var.names[f$about$side==by_name]
+  subformula <- clean_formula(formula, by_name)
+  result <- data |>
+    mutate(.groups=f$data[[by_name]]) |>
+    nest(.by=".groups") |>
+    arrange(.data$.groups) |>
+    mutate(.x=map(data, \(.x) {
+      paramsi <- params
+      paramsi$data <- .x
+      paramsi <- c(list(subformula), paramsi)
+      do.call(FUN, paramsi)
+    })) |> select(-"data") |>
+    unnest(".x")
+  if(by_name=="right") {
+    result <- result |> rename(value=".groups") |> mutate(variable=new.name)
+  } else {
+    result <- result |> rename(group.value=".groups") |> mutate(group=new.name)
+  }
+  as_atest(result)
+}
