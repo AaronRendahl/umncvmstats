3f4d773c448a745133a8ee92deb728affb37ca0a
Aaron Rendahl <rend0020@umn.edu>
2024-07-13 18:23:32 -0500

    move utils out of atest code

--- R/5915_generics.R
+++ R/5915_generics.R
@@ -1,6 +1,5 @@
 rm_class <- function(x, class) { class(x) <- setdiff(class(x), class); x }
 
-
 #' atest
 #'
 #' @param x XX
@@ -166,20 +165,6 @@ print.atest <- function(x, ...) {
   }
 }
 
-checkif2 <- function(x, require_two=TRUE) {
-  if(is.logical(x)) {
-    x <- factor(x, levels=c(FALSE, TRUE))
-  } else if(!is.factor(x)) {
-    x <- factor(x)
-  }
-  if(require_two) {
-    if(nlevels(x) > 2) stop("More than two unique levels found.")
-    if(nlevels(x) < 2) stop("Only one unique level found.")
-  }
-  if(length(x) == 0) stop("No non-missing values found.")
-  x
-}
-
 #' @importFrom broom tidy
 #' @importFrom tibble enframe
 #' @importFrom forcats as_factor
@@ -204,20 +189,3 @@ detach_about <- function(x) {
   x <- x |> select(-"about")
   list(result=x, about=notes)
 }
-
-
-capture_warnings <- function(expr, warnings=c(), replacement=warnings) {
-  w <- NULL
-  findwarnings <- function(w) {
-    m <- match(w$message, warnings)
-    m <- m[!is.na(m)]
-    if(length(m) > 0) {
-      w <<- replacement[m]
-      invokeRestart("muffleWarning")
-    }
-  }
-  result <- withCallingHandlers(expr, warning = findwarnings)
-  list(result=result, warnings=w)
-}
-
-
---
+++ R/utils.R
@@ -0,0 +1,27 @@
+capture_warnings <- function(expr, warnings=c(), replacement=warnings) {
+  w <- NULL
+  findwarnings <- function(w) {
+    m <- match(w$message, warnings)
+    m <- m[!is.na(m)]
+    if(length(m) > 0) {
+      w <<- replacement[m]
+      invokeRestart("muffleWarning")
+    }
+  }
+  result <- withCallingHandlers(expr, warning = findwarnings)
+  list(result=result, warnings=w)
+}
+
+checkif2 <- function(x, require_two=TRUE) {
+  if(is.logical(x)) {
+    x <- factor(x, levels=c(FALSE, TRUE))
+  } else if(!is.factor(x)) {
+    x <- factor(x)
+  }
+  if(require_two) {
+    if(nlevels(x) > 2) stop("More than two unique levels found.")
+    if(nlevels(x) < 2) stop("Only one unique level found.")
+  }
+  if(length(x) == 0) stop("No non-missing values found.")
+  x
+}
