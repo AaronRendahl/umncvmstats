09f834049af4dc4753a09bf3c351989f8c937a79
Aaron Rendahl <rend0020@umn.edu>
2024-07-17 15:20:32 -0500

    how to build homepage

--- notes.R
+++ notes.R
@@ -1,7 +1,9 @@
 devtools::document()
 devtools::build_readme() # slow, it installs package first
 devtools::build_vignettes()
+
 pkgdown::build_site()
 
+pkgdown::build_home()
 pkgdown::build_articles()
 pkgdown::build_tutorials()
