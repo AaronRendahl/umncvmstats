7954b3a779fed1b6c265901240c4afb1a6f9ba81
Aaron Rendahl <rend0020@umn.edu>
2024-07-18 08:22:19 -0500

    move combine tests

--- R/atest.R
+++ R/atest.R
@@ -1,27 +1,3 @@
-#' Combine multiple tests together
-#'
-#' @param ... XX
-#'
-#' @export
-combine_tests <- function(...) {
-  x <- list(...)
-  estimate.vars <- lapply(x, \(xi) attr(xi, "estimate.vars")) |> unlist() |> unique()
-  inference.vars <- lapply(x, \(xi) attr(xi, "inference.vars")) |> unlist() |> unique()
-  pri.vars <- lapply(x, \(xi) attr(xi, "pri.vars")) |> unlist() |> unique()
-  by.vars <- lapply(x, \(xi) attr(xi, "by.vars")) |> unlist() |> unique()
-  bind_rows(x) |> as_atest(estimate.vars=estimate.vars, inference.vars=inference.vars,
-                           pri.vars=pri.vars, by.vars=by.vars)
-}
-
-combine_tests_list <- function(x) {
-  estimate.vars <- lapply(x, \(xi) attr(xi, "estimate.vars")) |> unlist() |> unique()
-  inference.vars <- lapply(x, \(xi) attr(xi, "inference.vars")) |> unlist() |> unique()
-  pri.vars <- lapply(x, \(xi) attr(xi, "pri.vars")) |> unlist() |> unique()
-  by.vars <- lapply(x, \(xi) attr(xi, "by.vars")) |> unlist() |> unique()
-  bind_rows(x) |> as_atest(estimate.vars=estimate.vars, inference.vars=inference.vars,
-                           pri.vars=pri.vars, by.vars=by.vars)
-}
-
 as_atest <- function(x, ...) {
   UseMethod("as_atest")
 }
