5cbb50875eee7fc73a0a22d1df3920439550c3bd
Aaron Rendahl <rend0020@umn.edu>
2024-07-18 00:06:16 -0500

    rewritten simplify, much simpler. :)

--- R/simplify_atest.R
+++ R/simplify_atest.R
@@ -1,56 +1,51 @@
 
+#' @importFrom tidyr pivot_longer
 #' @importFrom rlang :=
 simplify_atest <- function(x) {
   xx <- separate_about(x)
   d <- xx$result
   a <- xx$about
 
-  nof <- function(var, group) {
-    n <- length(unique(d[[var]]))
-    if(group %in% names(d)) {
-      if(n == 0) stop(sprintf("Error: Found '%s' but no '%s'.", group, var))
-      n2 <- nrow(unique(d[c(var, group)]))
-      n <- c(n, n2)
-    }
-    n
-  }
-  n_group <- nof("group", "group.value")
-  n_response <- nof("response", "response.value")
-  n_variable <- nof("variable", "value")
-  n_terms <- nof("response", "model.terms")
-
-  pasteif <- function(a, b, sep) {
-    if_else(is.na(b), a, paste(a, b, sep=sep))
+  pasteif <- function(a, b, sep, FUN=\(a, b) paste(a, b, sep=sep)) {
+    if_else(is.na(b), a, FUN(a, b))
   }
 
-  .title <- NA
+  nn <- c(".y", ".y_value", ".y_contrast", ".terms", ".x", ".x_value", ".x_contrast", ".g", ".g_value")
+  empty <- rep(list(character()), length(nn)) |> setNames(nn) |> as_tibble()
+  d <- bind_rows(d, empty)
 
-  # we have a model, use that as either title or grouping factor
-  if(length(n_terms) > 1) {
-    d <- d |> mutate(.group=pasteif(.data$response, .data$model.terms, sep=" ~ "), .before=1)
-    # if just one model, use as title
-    if(length(unique(d$.group))==1) {
-      .title <- d$.group[1]
-      d$.group <- NULL
-    }
-    d <- d |> select(-c("response", "model.terms"))
+  if(any(with(d, !is.na(.y) & !is.na(.y_contrast)))) {stop("Internal error: can't have both .y and .y_contrast")}
+  if(any(with(d, !is.na(.x_value) & !is.na(.x_contrast)))) {stop("Internal error: can't have both .x_value and .x_contrast")}
+  if(any(with(d, !is.na(.terms) & !is.na(.y_contrast)))) {stop("Internal error: can't have both .terms and .y_contrast")}
+  if(any(with(d, !is.na(.terms) & (!is.na(.x) | !is.na(.x_value) | !is.na(.x_contrast))))) { stop("Internal error: can't have both .terms and an .x value") }
+  if(any(with(d, !is.na(.terms) & (!is.na(.g) | !is.na(.g_value))))) { stop("Internal error: can't have both .terms and a .g value") }
+  if(any(with(d, !is.na(.terms) & !is.na(.y_contrast)))) {stop("Internal error: can't have both .terms and .y_contrast")}
+
+  out <- d |> mutate(.Y = case_when(!is.na(.terms) ~ NA_character_,
+                                   !is.na(.y_value) ~ pasteif(.y, .y_value, " = "),
+                                   !is.na(.y_contrast) ~ pasteif(.y_contrast, .y_value, FUN=\(a, b) paste(b, a, sep=": ")),
+                                   TRUE ~ .y),
+                     .X = case_when(!is.na(.x_value) ~ pasteif(.x, .x_value, " = "),
+                                   !is.na(.x_contrast) ~ pasteif(.x_contrast, .x, FUN=\(a, b) paste(b, a, sep=": ")),
+                                   TRUE ~ .x),
+                     .G = pasteif(.g, .g_value, " = "),
+                     .M = if_else(!is.na(.terms), pasteif(pasteif(.y, .y_value, " = "), .terms, " ~ "), NA_character_),
+                     .after=".row") |>
+    select(-any_of(nn))
+
+  Nx <- out |> select(".Y", ".X", ".G", ".M") |> pivot_longer(everything()) |> summarize(n=length(unique(value[!is.na(value)])), .by="name")
+  Z <- Nx |> filter(n==0) |> pull(name)
+  N <- setNames(as.list(Nx$n), str_sub(Nx$name, 2, 2))
+
+  out <- out |> select(-any_of(Z))
+
+
+  if(N$M > 1) {                             # we have > 1 model, use that as grouping factor
+    out <- out |> rename(.GROUP = ".M")
+  } else if(N$G > 0) {                      # otherwise use grouping factor, if exists
+    out <- out |> rename(.GROUP = ".G")
   }
+  if(N$M == 1) out <- out |> select(-".M")  # if only one model, don't show model info
 
-  # we have values for the groups, combine them
-  if(n_group[1] > 0) {
-    d <- d |> mutate(.group=pasteif(.data$response, .data$model.terms, sep=" ~ "), .before=1)
-  }
-
-  # only one response / variable pair
-  if(n_response[1]==1 && n_variable[1]==1) {
-    .title <- paste(d$response[1], " ~ ", d$variable[1])
-    tmp <- d$response[1]
-    if(length(n_response)==2) d <- d |> rename({{tmp}}:="response.value")
-    tmp <- d$variable[1]
-    if(length(n_variable)==2) d <- d |> rename({{tmp}}:="value")
-    d <- d |> select(-any_of(c("response", "variable")))
-  }
-
-  if(!is.na(.title)) attr(d, "title") <- .title
-  d
+  out |> rename(any_of(c(response=".Y", variable=".X", group=".G", model=".M")))
 }
