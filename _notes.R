863d2bf3477b4eee6a235409b7d8c41d7525d482
Aaron Rendahl <rend0020@umn.edu>
2024-07-18 12:38:52 -0500

    more functions to know about

--- _notes.R
+++ _notes.R
@@ -1,9 +1,16 @@
+devtools::spell_check()
 devtools::document()
+devtools::check_man()
+
 devtools::build_readme() # slow, it installs package first
 devtools::build_vignettes()
 
-pkgdown::build_site()
+devtools::build_site()
 
+## subsets of build_site
 pkgdown::build_home()
 pkgdown::build_articles()
-pkgdown::build_tutorials()
+
+## uploads and checks elsewhere
+devtools::check_mac_release()
+devtools::check_win_release()
