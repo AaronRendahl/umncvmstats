082c325421f2f41b851b4ed6f4e05a58f2f0618b
Aaron Rendahl <rend0020@umn.edu>
2024-07-18 09:49:36 -0500

    data documentation

---
+++ R/data.R
@@ -0,0 +1,39 @@
+#' Motor Trend Car Road Tests
+#'
+#' A version of the Motor Trend car road tests data from the datasets package,
+#' but as a tibble with categorical variables stored as factors.
+#'
+#' @format A tibble with 32 rows and 12 variables.
+#'
+#' \describe{
+#' \item{model}{Make and model of the car}
+#' \item{mpg}{Miles/(US) gallon}
+#' \item{cyl}{Number of cylinders (as a factor)}
+#' \item{disp}{Displacement (cu.in.)}
+#' \item{hp}{Gross horsepower}
+#' \item{drat}{Rear axle ratio}
+#' \item{wt}{Weight (1000 lbs)}
+#' \item{qsec}{1/4 mile time}
+#' \item{vs}{Engine (V-shaped / straight)}
+#' \item{am}{Transmission (automatic / manual)}
+#' \item{gear}{Number of forward gears (as a factor)}
+#' \item{carb}{Number of carburetors (as a factor)}
+#' }
+#'
+#' @source The `datasets` package; see [datasets::mtcars].
+"mtcars2"
+
+#' Scores on pre/post test (generated data)
+#'
+#' Generated pre/post data to demonstrate paired tests.
+#' A score of 80 is considered passing.
+#'
+#' @format A tibble with 50 rows and 4 columns.
+#'
+#' \describe{
+#' \item{score1} Pre-test Score
+#' \item{score2} Post-test Score
+#' \item{pass1} Pre-test pass/fail status
+#' \item{pass2} Post-test pass/fail status
+#' }
+"passfail"
