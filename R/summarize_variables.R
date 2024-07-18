b5b68bad5d515032fe2021e54e1a4ab2e1a5d9b6
Aaron Rendahl <rend0020@umn.edu>
2024-07-18 08:29:04 -0500

    move gt functions

--- R/summarize_variables.R
+++ R/summarize_variables.R
@@ -1,37 +1,3 @@
-#' Set gt table padding
-#'
-#' @param x gt table
-#' @param table.spacing spacing, in pixels
-#'
-#' @return a gt.table with modified spacing
-#' @export
-#' @importFrom gt px
-#' @importFrom gt tab_options
-tab_padding <- function(x, table.spacing) {
-  sp <- px(table.spacing)
-  x |> tab_options(
-    data_row.padding = sp,
-    summary_row.padding = sp,
-    grand_summary_row.padding = sp,
-    footnotes.padding = sp,
-    source_notes.padding = sp,
-    row_group.padding = sp)
-}
-
-#' Compact a gt table
-#'
-#' @param x XX
-#' @param font.size XX
-#' @param spacing XX
-#'
-#' @return XX
-#' @export
-#' @importFrom gt tab_options
-tab_compact <- function(x, font.size=13, spacing=1) {
-  x |> tab_options(table.font.size = font.size) |>
-    tab_padding(table.spacing=spacing)
-}
-
 #' Summarize variables
 #'
 #' @param data XX
