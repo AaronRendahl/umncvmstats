cf08ccacf2d14493ea24d5327390b4539665d599
Aaron Rendahl <rend0020@umn.edu>
2024-07-19 09:46:21 -0500

    new way to handle logistic plot

--- R/geom_smooth_binomial.R
+++ R/geom_smooth_binomial.R
@@ -9,44 +9,29 @@ hide_y_axis <- function() {
         panel.grid.minor.y = element_blank())
 }
 
-glm1 <- function(formula, data,...) {
-  x <- match.call()
-  x[[1]] <- quote(glm)
-  out <- eval.parent(x)
-  class(out) <- c("glm1", class(out))
-  out
-}
-
-predictdf.glm1 <- function (model, xseq, se, level) {
-  ## from ggplot:::predictdf.glm
-  pred <- stats::predict(model, newdata = data_frame(x = xseq, .name_repair="minimal"),
-                         se.fit = se, type = "link")
-  out <- if (se) {
-    std <- stats::qnorm(level/2 + 0.5)
-    base::data.frame(x = xseq, y = model$family$linkinv(as.vector(pred$fit)),
-                     ymin = model$family$linkinv(as.vector(pred$fit - std * pred$se.fit)),
-                     ymax = model$family$linkinv(as.vector(pred$fit + std * pred$se.fit)),
-                     se = as.vector(pred$se.fit))
-  } else {
-    base::data.frame(x = xseq, y = model$family$linkinv(as.vector(pred)))
-  }
-  out$y <- out$y + 1
-  if(se) {
-    out$ymax <- out$ymax + 1
-    out$ymin <- out$ymin + 1
-  }
-  out
-}
-
 #' Add a smooth from a logistic regression
 #'
 #' @param ... XXX
 #'
 #' @importFrom stats binomial
 #' @export
-geom_smooth_binomial <- function(...) {
-  geom_smooth(aes(group=1),
-              method=glm1,
+geom_smooth_logistic <- function(...) {
+  geom_smooth(method=glm,
               formula=factor(y) ~ x,
               method.args=list(family=binomial()))
+}
+
+#' Make a binary scale
+#'
+#' @export
+scale_y_binary <- function(...) {
+  tob <- function(x) {
+    if(is.factor(x)) {
+      (x!=levels(x)[1])*1.0
+    } else {
+      x
+    }
+  }
+  scale_y_continuous(
+    transform=scales::new_transform("binary", tob, inverse=identity))
 }
