95aad94b2613e38f75290fda8946fbc6258fd56d
Aaron Rendahl <rend0020@umn.edu>
2024-07-27 19:30:06 -0500

    add paired documentation

--- R/5915_proportion2.R
+++ R/5915_proportion2.R
@@ -135,13 +135,20 @@ pairwise_proportion_test <- function(formula, data, adjust=c("bonferroni", "holm
 }
 
 #' Paired proportion test (McNemar's)
-#' @param formula XX
-#' @param data XX
-#' @param success XX
-#' @param alternative XX
-#' @param correct XX
-#' @param conf.level XX
-#' @param method XX
+#'
+#' Perform McNemar's test for paired proportions, by computing the proportion
+#' of those that switch from failure to success out of all those that switch
+#' in either direction and performing a one-sample proportion test with a null of 0.5.
+#'
+#' @param formula a formula of the form `y2 - y1 ~ 1` or `~ y2 -y1`, where `y1` and `y2` are numeric variables.
+#'     To perform test within subgroups, use `y2 - y1 ~ x` or `y2 - y1 ~ 1 | g`, or even `y2- y1 ~ x | g`,
+#'     where `x` and `g` are factor variables.
+#' @param data a data frame containing the values in the formula.
+#' @param success the level of the response variable to consider a success
+#' @param alternative character string specifying the alternative hypothesis, must be one of "`two.sided`" (default), "`greater`" or "`less`".
+#' @param correct a logical indicating whether Yates' continuity correction should be applied; used for Wilson test only.
+#' @param method character string specifying which method to use. One of "`default`", "`wilson`", or "`exact`".
+#' @param conf.level if desired, confidence level of the returned confidence interval. Must be a single number between 0 and 1.
 #'
 #' @export
 paired_proportion_test <- function(formula, data, success,
