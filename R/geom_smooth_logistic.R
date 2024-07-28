fb41375cb5de137edfc084fea8715618c64556d9
Aaron Rendahl <rend0020@umn.edu>
2024-07-28 11:23:38 -0500

    update documentation

--- R/geom_smooth_logistic.R
+++ R/geom_smooth_logistic.R
@@ -11,15 +11,26 @@ hide_y_axis <- function() {
 
 #' Add a smooth from a logistic regression
 #'
-#' @param ... XXX
+#' Add a smooth to a `ggplot`, using a logistic model.
 #'
+#' Uses `geom_smooth` with method `glm` and the `binomial` family, and a formula
+#' of `factor(y) ~ x`.
+#'
+#' @param mapping Set of aesthetic mappings created by [aes()]. If specified and
+#'   `inherit.aes = TRUE` (the default), it is combined with the default mapping at
+#'   the top level of the plot. You must supply mapping if there is no plot mapping.
+#' @param data The data to be displayed in this layer. See `ggplot::geom_smooth` for additional details.
+#' @param se logical specifying if confidence interval should be shown around the smooth.
+#' @param ... other arguments passed to `geom_smooth`.
 #' @importFrom stats glm
 #' @importFrom stats binomial
 #' @export
-geom_smooth_logistic <- function(...) {
-  geom_smooth(method=glm,
+geom_smooth_logistic <- function(mapping=NULL, data=NULL,
+                                 se=TRUE, ...) {
+  geom_smooth(mapping=mapping, data=data,
+              method=glm,
               formula=factor(y) ~ x,
-              method.args=list(family=binomial()))
+              method.args=list(family=binomial()), se=se, ...)
 }
 
 #' Make a binary scale
