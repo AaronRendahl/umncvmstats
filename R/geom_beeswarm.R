5e3bbc8f08a46f0c9ccd1c130e52577a01ac7ccd
Aaron Rendahl <rend0020@umn.edu>
2024-07-28 18:11:04 -0500

    move documentation to geom_beeswarm

--- R/geom_beeswarm.R
+++ R/geom_beeswarm.R
@@ -5,9 +5,65 @@
 #'  with a default of a compact swarm with random priority.
 #'  Uses the beeswarm package.
 #'
+#' @param data A data.frame containing plotting data in columns x and y.
+#' Usually obtained from data processed by ggplot2.
+
+#' @param method Method for arranging points (see Details below)
+#' @param spacing Scaling for adjusting point spacing (see [beeswarm::swarmx()]).
+#' Values between 1 (default) and 3 tend to work best.
+#' @param side Direction to perform jittering: 0: both directions;
+#' 1: to the right or upwards; -1: to the left or downwards.
+#' @param priority Method used to perform point layout (see Details below)
+#' @param fast Use compiled version of swarm algorithm? This option is ignored
+#' for all methods expect `"swarm"` and `"compactswarm"`.
+#' @param corral `string`. Method used to adjust points that would be placed to
+#' wide horizontally, default is `"none"`. See details below.
+#' @param corral.width `numeric`. Width of the corral, default is `0.9`.
+#'
+#' @details
+#' **method:** specifies the algorithm used to avoid overlapping points. The
+#' default `"swarm"` method places points in increasing order. If a point would
+#' overlap with an existing point, it is shifted sideways (along the group axis)
+#' by a minimal amount sufficient to avoid overlap.
+#'
+#' While the `"swarm"` method places points in a predetermined
+#' order, the `"compactswarm"` method uses a greedy strategy to determine which
+#' point will be placed next. This often leads to a more tightly-packed layout.
+#' The strategy is very simple: on each iteration, a point that can be placed as
+#' close as possible to the non-data axis is chosen and placed. If there are two
+#' or more equally good points, `priority` is used to break ties.
+#'
+#' The other 3 methods first discretise the values along the data axis, in order
+#' to create more efficient packing. The `"square"` method places points on a
+#' square grid, whereas `"hex"` uses a hexagonal grid. `"centre"`/`"center"`
+#' uses a square grid to produce a symmetric swarm. The number of break points
+#' for discretisation is determined by a combination of the available plotting
+#' area and the `spacing` argument.
+#'
+#' **priority:** controls the order in which points are placed, which generally
+#' has a noticeable effect on the plot appearance. `"ascending"` gives the
+#' 'traditional' beeswarm plot. `"descending"` is the opposite. `"density"`
+#' prioritizes points with higher local density. `"random"` places points in a
+#' random order. `"none"` places points in the order provided.
+#'
+#' **corral:** By default, swarms from different groups are not prevented from
+#' overlapping, i.e. `"corral = "none"`. Thus, datasets that are very large or
+#' unevenly distributed may produce ugly overlapping beeswarms. To control
+#' runaway points one can use the following methods. `"gutter"` collects runaway
+#' points along the boundary between groups. `"wrap"` implement periodic boundaries.
+#' `"random"` places runaway points randomly in the region. `"omit"` omits runaway
+#' points.
+#'
+#' @keywords internal
+#' @importFrom beeswarm swarmx
+#' @importFrom rlang abort
+#' @importFrom rlang warn
+#' @importFrom rlang inform
+#' @seealso [geom_beeswarm()], [ggbeeswarm::position_quasirandom()],
+#' [beeswarm::swarmx()]
+#'
 #' @inheritParams ggplot2::geom_point
 #' @inheritParams position_beeswarm
-#' @inheritParams offset_beeswarm
 #' @import ggplot2
 #' @seealso
 #'  [ggbeeswarm::geom_quasirandom()] an alternative method,
