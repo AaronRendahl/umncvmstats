15f8bea5d58a09432f44a6ff9644e35230a9363b
Aaron Rendahl <rend0020@umn.edu>
2024-07-28 13:51:04 -0500

    to update DESCRIPTION file

--- _notes.R
+++ _notes.R
@@ -20,4 +20,5 @@ devtools::check_win_release()
 pkgload::dev_help('one_proportion_test')
 
 # https://github.com/ThinkR-open/prepare-for-cran
+attachment::att_amend_desc()
 checkhelper::find_missing_tags()
