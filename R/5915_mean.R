8abb4ab03abc845fceae4a84d3462fdbea40b2da
Aaron Rendahl <rend0020@umn.edu>
2024-07-27 19:23:41 -0500

    paired t-test documentation

--- R/5915_mean.R
+++ R/5915_mean.R
@@ -131,12 +131,20 @@ pairwise_t_test <- function(formula, data, adjust=c("bonferroni", "holm", "none"
 
 #' Paired t-test
 #'
-#' @param formula XX
-#' @param data XX
-#' @param alternative XX
-#' @param null XX
-#' @param conf.level XX
-#' @param backtransform XX
+#' Compute the mean difference between paired samples, the corresponding
+#' confidence interval, and a p-value for the null hypothesis of equal means
+#' (that is, a difference of zero).
+#'
+#' @param formula a formula of the form `y2 - y1 ~ 1` or `~ y2 -y1`, where `y1` and `y2` are numeric variables.
+#'     To perform test within subgroups, use `y2 - y1 ~ x` or `y2 - y1 ~ 1 | g`, or even `y2- y1 ~ x | g`,
+#'     where `x` and `g` are factor variables.
+#' @param data a data frame containing the values in the formula.
+#' @param alternative character string specifying the alternative hypothesis, must be one of "`two.sided`" (default), "`greater`" or "`less`".
+#' @param null a number specifying the null proportion for testing a null hypothesis; if not specified, no hypothesis test is performed.
+#' @param conf.level confidence level of the returned confidence interval. Must be a single number between 0 and 1.
+#' @param backtransform if response variable is of form `log(y2) - log(y1)`, backtransform
+#'     the resulting estimate and confidence interval bounds, so that they
+#'     report the ratio of the geometric means rather than the difference on the log scale.
 #'
 #' @export
 paired_t_test <- function(formula, data,
