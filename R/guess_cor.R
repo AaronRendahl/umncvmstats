a55a429ed0655e23622a6d370a75cbffd4239cce
Aaron Rendahl <rend0020@umn.edu>
2024-07-28 12:41:46 -0500

    add documentation

--- R/guess_cor.R
+++ R/guess_cor.R
@@ -1,3 +1,13 @@
+#' Create a random scatterplot for guessing correlations
+#'
+#'
+#' @param seed the random seed used in creating the data.
+#' @param nonp should non-parametric correlations be shown?
+#' @param skew should extra skew be randomly added?
+#' @param rank should the data set be converted to ranks before plotting and computing correlations?
+#' @param answer option to control interactivity; options are "`interactive`", "`show`", and "`hide`".
+#' @param return.data normally the plot is returned; if TRUE, the data is returned instead.
+#'
 #' @export
 #' @importFrom stats rbeta
 #' @importFrom stats runif
