6ae6acebdc4afc73d8284b5bed7b446fa3c27f65
Aaron Rendahl <rend0020@umn.edu>
2024-07-28 14:32:09 -0500

    update for gtsummary 2.0

--- R/descriptive_statistics.R
+++ R/descriptive_statistics.R
@@ -16,13 +16,14 @@
 #' @importFrom gtsummary tbl_summary
 #' @importFrom gtsummary as_gt
 #' @importFrom gtsummary all_continuous
+#' @importFrom gtsummary all_categorical
 #' @importFrom gtsummary with_gtsummary_theme
 descriptive_statistics <- function(data, ..., digits=2, compact=TRUE) {
   theme_here <- list(
     "tbl_summary-arg:digits" = all_continuous() ~ digits,
-    "tbl_summary-str:categorical_stat" = "{n}/{N} ({p}%)",
     "tbl_summary-str:default_con_type" = "continuous2",
-    "tbl_summary-str:continuous_stat" = c("{mean} \u00B1 {sd}", "{median} ({min}, {max})"))
+    "tbl_summary-arg:statistic" = list(all_continuous() ~ c("{mean} \u00B1 {sd}", "{median} ({min}, {max})"),
+                                       all_categorical() ~ "{n}/{N} ({p}%)"))
   out <- with_gtsummary_theme(theme_here, tbl_summary(data, ...)) |> as_gt()
   if(compact) {
     out <- out |> tab_compact()
