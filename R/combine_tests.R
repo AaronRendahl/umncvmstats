51fdb147577df4a721677af08ede4aeca7c63bb4
Aaron Rendahl <rend0020@umn.edu>
2024-08-19 19:54:00 -0500

    add about.vars to combine_tests

--- R/combine_tests.R
+++ R/combine_tests.R
@@ -5,19 +5,14 @@
 #' @export
 combine_tests <- function(...) {
   x <- list(...)
-  estimate.vars <- lapply(x, \(xi) attr(xi, "estimate.vars")) |> unlist() |> unique()
+  about.vars <- lapply(x, \(xi) attr(xi, "about.vars")) |> unlist() |> unique()
   inference.vars <- lapply(x, \(xi) attr(xi, "inference.vars")) |> unlist() |> unique()
   pri.vars <- lapply(x, \(xi) attr(xi, "pri.vars")) |> unlist() |> unique()
   by.vars <- lapply(x, \(xi) attr(xi, "by.vars")) |> unlist() |> unique()
-  bind_rows(x) |> as_atest(estimate.vars=estimate.vars, inference.vars=inference.vars,
+  bind_rows(x) |> as_atest(about.vars=about.vars, inference.vars=inference.vars,
                            pri.vars=pri.vars, by.vars=by.vars)
 }
 
 combine_tests_list <- function(x) {
-  estimate.vars <- lapply(x, \(xi) attr(xi, "estimate.vars")) |> unlist() |> unique()
-  inference.vars <- lapply(x, \(xi) attr(xi, "inference.vars")) |> unlist() |> unique()
-  pri.vars <- lapply(x, \(xi) attr(xi, "pri.vars")) |> unlist() |> unique()
-  by.vars <- lapply(x, \(xi) attr(xi, "by.vars")) |> unlist() |> unique()
-  bind_rows(x) |> as_atest(estimate.vars=estimate.vars, inference.vars=inference.vars,
-                           pri.vars=pri.vars, by.vars=by.vars)
+  do.call(combine_tests, x)
 }
