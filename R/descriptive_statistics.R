7c89751f20ea683a29309bf185d47f5aafe581cb
Aaron Rendahl <rend0020@umn.edu>
2024-08-20 18:13:16 -0500

    versions of min/max that don't throw a warning if all are missing

--- R/descriptive_statistics.R
+++ R/descriptive_statistics.R
@@ -22,11 +22,30 @@ descriptive_statistics <- function(data, ..., digits=2, compact=TRUE) {
   theme_here <- list(
     "tbl_summary-arg:digits" = all_continuous() ~ digits,
     "tbl_summary-str:default_con_type" = "continuous2",
-    "tbl_summary-arg:statistic" = list(all_continuous() ~ c("{mean} \u00B1 {sd}", "{median} ({min}, {max})"),
+    "tbl_summary-arg:statistic" = list(all_continuous() ~ c("{mean} \u00B1 {sd}", "{median} ({minX}, {maxX})"),
                                        all_categorical() ~ "{n}/{N} ({p}%)"))
-  out <- with_gtsummary_theme(theme_here, tbl_summary(data, ...)) |> as_gt()
+  out <- with_gtsummary_theme(theme_here, tbl_summary(data, ...)) |>
+    add_stat_label(label=all_continuous() ~ c("Mean \u00B1 SD", "Median (Min, Max)")) |>
+    as_gt()
   if(compact) {
     out <- out |> tab_compact()
   }
   out
+}
+
+# tibble(a=1:10,b=rnorm(10)) |>
+#   tbl_summary(type = all_continuous() ~ "continuous2",
+#               statistic = all_continuous() ~ c("{mean} \u00B1 {sd}", "{median} ({minX}, {maxX})")) |>
+#   add_stat_label(label=all_continuous() ~ c("Mean \u00B1 SD", "Median (Min, Max)"))
+
+minX <- function(x) {
+  x <- x[!is.na(x)]
+  n <- length(x)
+  if(n<=1) NA else min(x)
+}
+
+maxX <- function(x) {
+  x <- x[!is.na(x)]
+  n <- length(x)
+  if(n<=1) NA else max(x)
 }
